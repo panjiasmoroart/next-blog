477b9451743aff4f75ffdf513f3e916ee2e01f3a
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-03-16 12:50:56 +0700

    Design Frontend Header and Menu - Part 2

--- src/components/Header/TopNav/TopNav.jsx
+++ src/components/Header/TopNav/TopNav.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import * as Icon from '@phosphor-icons/react/dist/ssr'
  
  const TopNav = () => {
      return (
-         <div>
-             Top Nav
-         </div>
+        <>
+            <div className='bg-slate-600'>
+                <div className='container flex items-center justify-between h-[44px]'>
+                    <div className='left-block flex items-center'>
+                        <div className='location flex items-center max-lg:hidden'>
+                    <Icon.MapPin className='text-white text-xl' />
+                    <span className='ml-2 caption1 text-white'>160 Boradway 12th floor, New York</span> 
+                        </div>
+        
+                    <div className='mail lg:ml-7 flex items-center'>
+                        <Icon.Envelope className='text-white text-xl'/>
+                        <span className='ml-2 caption1 text-white'>https://github.com/panjiasmoroart</span>  
+                    </div>
+        
+                    </div>
+                </div>
+            </div>
+        </>
      );
  };
  
