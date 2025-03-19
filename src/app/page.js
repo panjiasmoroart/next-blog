fae7fa5e4fea2b35447891fb5ee70900691f061c
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-03-20 06:48:07 +0700

    Latest Blog Page Design - Part 1

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,14 @@ import Menu from "@/components/Header/Menu/Menu";
 import TopNav from "@/components/Header/TopNav/TopNav";
 import Slider from "@/components/Slider/Slider";
 import Service from "@/components/Service/Service";
-import serviceData from '@/data/service.json';
 import PaymentGateway from "@/components/PaymentGateway/PaymentGateway";
 import CaseStudy from "@/components/CaseStudy/CaseStudy";
 import PaymentGatewayTwo from "@/components/PaymentGateway/PaymentGatewayTwo";
 import FormRequest from "@/components/FormRequest/FormRequest";
 import Testimonial from "@/components/Testimonial/Testimonial";
+import serviceData from '@/data/service.json';
+import blogData from '@/data/blog.json'
+import Blog from "@/components/Blog/Blog";
 
 export default function Home() {
   return (
@@ -26,6 +28,7 @@ export default function Home() {
           <PaymentGatewayTwo />
           <FormRequest />
           <Testimonial />
+          <Blog data={blogData} />
        </main>
     </div>
   );
