fb65f84e020925e330beafe9dd19c71796f3662f
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-03-19 15:06:12 +0700

    Gate Way Services Part Design - Part 2

--- src/components/PaymentGateway/PaymentGatewayTwo.jsx
+++ src/components/PaymentGateway/PaymentGatewayTwo.jsx
@@ -28,7 +28,36 @@ const PaymentGatewayTwo = () => {
                         </div>
                 
                         <div className='w-11/12 xl:w-7/12'>
-                            sadfasdfsadf
+                            <div className='right pl-10' style={{  transform: isInView ? "none" : 'translateY(60px)', 
+                                opacity: isInView ? 1 : 0, transition: 'all 0.7s cubic-bezier(0.17, 0.55, 0.55,1) 0.3s', }}>
+                        
+                                <div className='bg-img'>
+                                    <Image width={5000} height={5000} className='w-full' src="/images/gateway2-bg.webp" alt='image' /> 
+                                </div>
+                                <div className='feature-item py-4 px-6 rounded-2xl bg-white inline-flex items-center gap-4 box-shadow'>
+                                    <i className='icon-list text-2xl p-4 rounded-2xl bg-red-400'></i>
+                                    <div className='text'>
+                                        <div className='heading7'>2K+</div>
+                                        <div className='heading7 text-secondary'>Projects</div> 
+                                    </div> 
+                                </div>
+                        
+                                <div className='feature-item py-4 px-6 rounded-2xl bg-white inline-flex items-center gap-4 box-shadow'>
+                                    <Icon.Star weight='fill' className='text-yellow-600 text-3xl'/>
+                                    <div className='text'>
+                                        <div className='heading7'>4.8</div>
+                                        <div className='heading7 text-secondary'>Satisfaction</div>
+                                    </div> 
+                                </div>
+                        
+                                <div className='feature-item py-4 px-6 rounded-2xl bg-white inline-flex items-center gap-4 box-shadow'>
+                                    <i className='icon-user text-2xl p-4 rounded-2xl bg-red-600'></i>
+                                    <div className='text'>
+                                        <div className='heading7'>6 Years</div>
+                                        <div className='heading7 text-secondary'>Product Designer</div> 
+                                    </div> 
+                                </div>
+                            </div>
                         </div>
                     </div> 
                 </div>
