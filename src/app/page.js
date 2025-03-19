e3d7fc312c929ddb336c0ab507d56b56c9b74cd1
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-03-19 07:14:46 +0700

    Design Frontend Slider Option - Part 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,21 @@
 import Image from "next/image";
 import Menu from "@/components/Header/Menu/Menu";
 import TopNav from "@/components/Header/TopNav/TopNav";
+import Slider from "@/components/Slider/Slider";
+import Service from "@/components/Service/Service";
 
 export default function Home() {
   return (
     <div className="overflow-x-hidden">
-       <header id="header">
-           <TopNav />
-           <Menu />
-       </header>
+      <header id="header">
+        <TopNav />
+        <Menu />
+      </header>
+
+       <main className="content">
+          <Slider />
+          <Service />
+       </main>
     </div>
   );
 }
