343363c6c59f039a7c826c0c265f8e27bbb2d571
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-03-20 07:42:57 +0700

    About US Page Design - Part 3

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import Footer from '@/components/Footer/Footer';
  import Menu from '@/components/Header/Menu/Menu';
  import TopNav from '@/components/Header/TopNav/TopNav';
  import Partner from '@/components/Partner/Partner';
+import AboutSection from '@/components/Section/AboutSection';
  import Breadcrumb from '@/components/Section/Breadcrumb';
- import React from 'react';
  
 const AboutPage = () => {
     return (
@@ -15,6 +16,7 @@ const AboutPage = () => {
         
             <main className="content">
                 <Breadcrumb link="About Us" img="/images/header.webp" title='About Us' desc="The jobs report soundly beat expectations, with job gains broadly spread across the economy and about 60% higher" />
+                <AboutSection />
             </main>
         
             <Partner className='lg:mt-[100px] sm:mt-16 mt-10' /> 
