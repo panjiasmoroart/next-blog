8cfa0944e06af12d9422fe13acda62b191a346c2
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-03-20 14:52:40 +0700

    Services and Services Details Page Design - Part 1

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -10,7 +10,7 @@ const Service = ({ data }) => {
 
      return (
         <div>
-             <section className='service-block lg:mt-[100px] sm:mt-16 mt-10' ref={ref}>
+              <section className='service-block lg:mt-[100px] sm:mt-16 mt-10 mb-6' ref={ref}>
                 <div className='container'>
                     <h3 className='heading3 text-center'>Our Sevices</h3>
                     <div className='list-service grid lg:grid-cols-3 sm:grid-cols-2 gap-8 md:mt-10 mt-6 gap-y-10' style={{ transform: isInView ? "none" : 'translateY(60px)', opacity: isInView ? 1 : 0, transition: 'all 0.7s cubic-bezier(0.17, 0.55, 0.55,1) 0.3s', }}>
