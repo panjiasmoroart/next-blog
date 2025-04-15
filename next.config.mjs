fa23ca4b48bad066ce435f2416e57ce3c6316ed0
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-04-15 14:13:52 +0700

    Call Backend API in Nextjs Frontend

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // sassOptions: {
-    //     silenceDeprecations: ['legacy-js-api'],
-    // }
+    images: {
+        domains: ['127.0.0.1','localhost'] 
+    }
 };
 
 export default nextConfig;
