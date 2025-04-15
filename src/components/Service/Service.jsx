70d20500803b7d4a1a75f73ceb7a8c95df7aadc3
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-04-15 17:11:56 +0700

    Call Services API to Frontend Page - Part 2

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -3,6 +3,7 @@ import { useInView } from 'framer-motion';
 import React, { useEffect, useRef, useState } from 'react';
 import ServiceItem from './Item/ServiceItem';
 import { API_BASE_URL } from '@/config/config';
+import ClipLoader from 'react-spinners/ClipLoader';
   
 const Service = ( ) => {
     const ref = useRef(null)
@@ -32,13 +33,21 @@ const Service = ( ) => {
             <section className='service-block lg:mt-[100px] sm:mt-16 mt-10 mb-6' ref={ref}>
                 <div className='container'>
                     <h3 className='heading3 text-center'>Our Sevices</h3>
-                    <div className='list-service grid lg:grid-cols-3 sm:grid-cols-2 gap-8 md:mt-10 mt-6 gap-y-10' style={{ transform: isInView ? "none" : 'translateY(60px)', opacity: isInView ? 1 : 0, transition: 'all 0.7s cubic-bezier(0.17, 0.55, 0.55,1) 0.3s', }}>
-                        {
-                            services.slice(0,6).map((item, index) => (
-                                <ServiceItem data={item} key={index} number={index} />
-                            ))
-                        }
-                    </div>
+                    {
+                        loading ? ( 
+                            <div className='flex justify-center items-center h-[500px]'>
+                                <ClipLoader color='#3498db' size={50} />
+                            </div>
+                        ): (
+                            <div className='list-service grid lg:grid-cols-3 sm:grid-cols-2 gap-8 md:mt-10 mt-6 gap-y-10' style={{ transform: isInView ? "none" : 'translateY(60px)', opacity: isInView ? 1 : 0, transition: 'all 0.7s cubic-bezier(0.17, 0.55, 0.55,1) 0.3s', }}>
+                                {
+                                    services.slice(0,6).map((item, index) => (
+                                        <ServiceItem data={item} key={index} number={index} />
+                                    ))
+                                }
+                            </div>
+                        )
+                    }        
                 </div>
             </section>
         </div>
